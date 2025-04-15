276e8e8124595a8fd05649092df82b7844b5a5de
xxxbarry <127599710+xxxbarry@users.noreply.github.com>
2025-04-15 17:59:51 -0400

    Update Tokens.ts

--- src/components/Tokens.ts
+++ src/components/Tokens.ts
@@ -5,62 +5,176 @@ export type Token = {
     decimals: number,
 }
 
-// ROPSTEN TEST NETWORK TOKEN ADDRESSES
+// ARBITRUM NETWORK TOKEN ADDRESSES
 const Tokens: Array<Token> = [
-    {
-        imgPath: "/crypto/eth.svg",
-        symbol: "ETH",
-        address: process.env.NEXT_PUBLIC_TOKEN_A_ADDR || '',
-        decimals: 18
-    },
-    {
-        imgPath: "/crypto/bat.svg",
-        symbol: "BAT",
-        address: process.env.NEXT_PUBLIC_TOKEN_B_ADDR || '',
-        decimals: 18
-    },
-    {
-        imgPath: "/crypto/uni.svg",
-        symbol: "UNI",
-        address: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
-        decimals: 18
-    },
-    {
-        imgPath: "/crypto/usdt.svg",
-        symbol: "USDT",
-        address: "0x110a13FC3efE6A245B50102D2d79B3E76125Ae83",
-        decimals: 6
-    },
-    {
-        imgPath: "/crypto/usdc.svg",
-        symbol: "USDC",
-        address: "0x07865c6E87B9F70255377e024ace6630C1Eaa37F",
-        decimals: 6
-    },
-    {
-        imgPath: "/crypto/link.svg",
-        symbol: "LINK",
-        address: "0x514910771AF9Ca656af840dff83E8264EcF986CA",
-        decimals: 18
-    },
-    {
-        imgPath: "/crypto/wbtc.svg",
-        symbol: "WBTC",
-        address: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
-        decimals: 8
-    },
-    {
-        imgPath: "/crypto/comp.svg",
-        symbol: "COMP",
-        address: "0xf76D4a441E4ba86A923ce32B89AFF89dBccAA075",
-        decimals: 18
-    },
+   
+  
     {
         imgPath: "/crypto/dai.svg",
         symbol: "DAI",
         address: "0x31F42841c2db5173425b5223809CF3A38FEde360",
         decimals: 18
     },
-]
+    {
+        imgPath: "/crypto/usdc.svg",
+        symbol: "USDC",
+        address: "0x9e90296a1343258388a6476db1c12d76dd04504e",
+        decimals: 18
+    },
+    {
+        imgPath: "/crypto/zap.svg",
+        symbol: "ZAP",
+        address: "0xb2785e03e542161d3dfa3ce3ddad4a26d892cd6a",
+        decimals: 18
+    },
+    {
+        imgPath: "/crypto/gmx.svg",
+        symbol: "GMX",
+        address: "0xfc5a1a6eb076a2c7ad06ed22c90d7e710e35ad0a",
+        decimals: 18
+    },
+    {
+        imgPath: "/crypto/usde.svg",
+        symbol: "USDe",
+        address: "0x5d3a1ff2b6bab83b63cd9ad0787074081a52ef34",
+        decimals: 18
+    },
+    {
+        imgPath: "/crypto/link.svg",
+        symbol: "LINK",
+        address: "0xf97f4df75117a78c1a5a0dbb814af92458539fb4",
+        decimals: 18
+    },
+    {
+        imgPath: "/crypto/arb.svg",
+        symbol: "ARB",
+        address: "0xb41877593e1eec5a380f8c22d0abfa17b0865527",
+        decimals: 8
+    },
+    {
+        imgPath: "/crypto/aave.svg",
+        symbol: "aArbwstETH",
+        address: "0x513c7e3a9c69ca3e22550ef58ac1c0088e918fff",
+        decimals: 18
+    },
+    {
+        imgPath: "/crypto/wsteth.svg",
+        symbol: "wstETH",
+        address: "0x5979d7b546e38e414f7e9822514be443a4800529",
+        decimals: 18
+    },
+    {
+        imgPath: "/crypto/pendle.svg",
+        symbol: "PENDLE",
+        address: "0x7e6f08984feac6244aec5dce3bd66387e82b9630",
+        decimals: 18
+    },
+    {
+        imgPath: "/crypto/usdt.svg",
+        symbol: "USD₮0",
+        address: "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9",
+        decimals: 6
+    },
+    {
+        imgPath: "/crypto/usdc.svg",
+        symbol: "USDC",
+        address: "0xf31e1ae27e7cd057c1d6795a5a083e0453d39b50",
+        decimals: 0
+    },
+    {
+        imgPath: "/crypto/ryan.svg",
+        symbol: "RYAN",
+        address: "0xf67ec0a483a1c80eb1e92d58e46f1c10f8383b8d",
+        decimals: 2
+    },
+    {
+        imgPath: "/crypto/slp.svg",
+        symbol: "SLP",
+        address: "0x187acd192822aed86d89ad516e4a3c3bb1929574",
+        decimals: 18
+    },
+    {
+        imgPath: "/crypto/arb.svg",
+        symbol: "ARB",
+        address: "0x912ce59144191c1204e64559fe8253a0e49e6548",
+        decimals: 18
+    },
+    {
+        imgPath: "/crypto/wbarb.svg",
+        symbol: "WBARB",
+        address: "0x7b6c14ccca515d6d1f234054254bdced56ac173e",
+        decimals: 18
+    },
+    {
+        imgPath: "/crypto/wbtc.svg",
+        symbol: "WBTC",
+        address: "0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f",
+        decimals: 8
+    },
+    {
+        imgPath: "/crypto/eth.svg",
+        symbol: "ETH",
+        address: "0xf810fc6501a91e3e7ff43f65fd81335238bbbd5e",
+        decimals: 18
+    },
+    {
+        imgPath: "/crypto/weth.svg",
+        symbol: "WETH",
+        address: "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
+        decimals: 18
+    },
+    {
+        imgPath: "/crypto/etherfast.svg",
+        symbol: "ETHERFAST",
+        address: "0x94f1747558e1287a89bf4be3abb6e736d8b5fc38",
+        decimals: 18
+    },
+    {
+        imgPath: "/crypto/rdnt.svg",
+        symbol: "RDNT",
+        address: "0x3082cc23568ea640225c2467653db90e9250aaa0",
+        decimals: 18
+    },
+    {
+        imgPath: "/crypto/aave.svg",
+        symbol: "AAVE",
+        address: "0xba5ddd1f9d7f570dc94a51479a000e3bce967196",
+        decimals: 18
+    },
+    {
+        imgPath: "/crypto/ethfi.svg",
+        symbol: "ETHFI",
+        address: "0x7189fb5b6504bbff6a852b13b7b82a3c118fdc27",
+        decimals: 18
+    },
+    {
+        imgPath: "/crypto/usdct.svg",
+        symbol: "USDCT",
+        address: "0xaf0807b1bfb6c9a6a000c393fa152322a67bb34b",
+        decimals: 18
+    },
+    {
+        imgPath: "/crypto/trump.svg",
+        symbol: "gettrumps",
+        address: "0xf224dbc383874ecf3d8c1a63736fd914c30448a3",
+        decimals: 10
+    },
+    {
+        imgPath: "/crypto/dai.svg",
+        symbol: "DAI",
+        address: "0x050d4e9ca843a9640e8f92731833805c650f5e3d",
+        decimals: 18
+    },
+    {
+        imgPath: "/crypto/usdt.svg",
+        symbol: "USDT",
+        address: "0x422e3b839e0bcf7c9dbc7df39329e9380096149c",
+        decimals: 18
+    }
+];
+
+
+export default Tokens
+
 
 export default Tokens
